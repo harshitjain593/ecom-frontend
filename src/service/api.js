729449bc001fac0a8746f1bca2e1ef65bab07d77
harshitjain593 <harshitj593@gmail.com>
2025-10-10 13:07:15 +0530

    backend api port changed

--- src/service/api.js
+++ src/service/api.js
@@ -1,6 +1,6 @@
 
 // export const API_URL =`https://api.shinedecoration.com`
-export const API_URL ='http://16.170.204.87:5001';
+export const API_URL ='http://13.53.42.47:5001';
 
 // 'http://35.154.132.235:5000'
 
